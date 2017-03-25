7fa9115f4946d0a5a45676728216234695fd1b2e
Ildar Saribzhanov <ildar.r.saribzhanov@gmail.com>
2017-03-25 10:25:42 +0300

    [Update] finished processing: Кажется оно работает

--- flack2mp3.sh
+++ flack2mp3.sh
@@ -7,10 +7,6 @@ function get_id3_in_flac()
 {
 	flac_file="$1"
 
-	echo 'Flac file!!!!!!'
-	echo "$flac_file"
-	echo 'Flac file end!!!!!!'
-
 	metaflac --export-tags-to - "$flac_file" | while read -d $'\n' tag; do
 		tag_name=$(echo "$tag" | awk -F= '{ print $1 }')
 		tag_value=$(echo "$tag" | awk -F= '{ print $2 }' | sed 's/"/\\"/g')
@@ -63,48 +59,61 @@ function main()
 	output_dir=$2
 	lame_opts=$3
 
-	# # Надо указывать папки исходную и назначения
-	# if [[ -z "$input_dir" || -z "$output_dir" ]]; then
-	# 	echo "Usage: $0 <input_dir> <output_dir> [lame_opts]"
-	# 	echo "Example: $0 /path/flac/albums /path/need/mp3 \"-b 320 -h\""
-	# 	exit 1
-	# fi
+	# Надо указывать папки исходную и назначения
+	if [[ -z "$input_dir" || -z "$output_dir" ]]; then
+		echo "Usage: $0 <input_dir> <output_dir> [lame_opts]"
+		echo "Example: $0 /path/flac/albums /path/need/mp3 \"-b 320 -h\""
+		exit 1
+	fi
 
-	# # Опции по умолчанию, если не установлены явно
-	# if [[ -z "$lame_opts" ]]; then
-	# 	lame_opts=$DEFAULT_LAME_OPTS
-	# fi
+	# Опции по умолчанию, если не установлены явно
+	if [[ -z "$lame_opts" ]]; then
+		lame_opts=$DEFAULT_LAME
+	fi
 
-	# # Существование исходной папки
-	# if [ ! -d "$input_dir" ]; then
-	# 	echo "<input_dir> do not exists"
-	# 	return 1
-	# fi
+	# Существование исходной папки
+	if [ ! -d "$input_dir" ]; then
+		echo "<input_dir> do not exists"
+		return 1
+	fi
 
-	# # Создание папки назначения при её отсутствии
-	# if [ ! -d "$output_dir" ]; then
-	# 	echo "<output_dir> do not exists. Create <output_dir>"
-	# 	mkdir -p "$output_dir"
-	# fi
+	# Создание папки назначения при её отсутствии
+	if [ ! -d "$output_dir" ]; then
+		echo "<output_dir> do not exists. Create <output_dir>"
+		mkdir -p "$output_dir"
+	fi
 
 	# Определяем как будут разделяться строки. Нам нужны только переносы, чтобы игнирировлись пробелы
 	OIFS=$IFS; IFS=$'\n'
 
+
 	# Перебираем в цикле файлы с расширением flac, отсортированные по имени
 	for f in $(find "$input_dir" -name "*.flac" | sort); do
-		flac_base=`basename "$f"`
-		echo "$flac_base"
-		# new_mp3="${f%.*}".mp3
-		# id3_tags=$(get_id3_in_flac "$f")
-		# eval "flac -cd \"$f\" | lame $DEFAULT_LAME_OPTS $id3_tags - \"$new_mp3\""; 
+		flac_file_name=`basename "$f"`
+		
+		# файл с заменой
+		new_mp3_file=$output_dir${f/${input_dir}/}
+
+		# создадим нужную папку для mp3
+		need_mp3_dir=${new_mp3_file/${flac_file_name}/}
+
+		if [ ! -d "$need_mp3_dir" ]; then
+			echo "Create dir - '$need_mp3_dir'"
+			mkdir -p "$need_mp3_dir"
+		fi
+
+		# имя файла заканчивается на mp3
+		new_mp3_file="${new_mp3_file%.*}".mp3
+
+		id3_tags=$(get_id3_in_flac "$f")
+
+		# Выполнение
+		eval "flac -cd \"$f\" | lame $lame_opts $id3_tags - \"$new_mp3_file\""
 	done
 }
 
 
 echo 'Start processing'
-
-# res=$(get_id3_in_flac "/home/ildar/download/shs/02 - Slam.flac")
-# echo "-$res-"
 
 main "$@"
 
